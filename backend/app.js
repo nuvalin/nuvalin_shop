fe1368d1b74c876c13f2f163478757fca74c2c3c
nuvalin <nuvzmoodley@gmail.com>
2024-01-29 01:53:07 +0200

    COMMIT

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use('/api/v1', auth)
 app.use('/api/v1', payment)
 app.use('/api/v1', order)
 
- if (process.env.NODE_ENV !== 'PRODUCTION') {
+ if (process.env.NODE.ENV !== 'PRODUCTION') {
      app.use(express.static(path.join(__dirname, '../frontend/build')))
 
      app.get('*', (req, res) => {
